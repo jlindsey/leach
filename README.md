9d3baa04bb88c3f4c754f9cd6c526e31839a35ce
Josh Lindsey <Joshua.Lindsey@georgetown.edu>
2018-10-30 16:20:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+Porter
+======
+
+Automated, Consul-driven service for LetsEncrypt certificate requests and renewals.
+
+Setup
+-----
+
+To bootstrap the tool, a config key needs to be added to Consul's KV store. The `{PREFIX}` in
+the list below is customizable with the `CONSUL_PREFIX` env var or flag and by default is `porter`.
+
+`{PREFIX}/conf` must be a JSON doc like so:
+
+    {
+        "email": "test@example.com",
+        "ident": {
+            "country": ["US"],
+            "province": ["Virginia"],
+            "locality": ["Arlington"],
+            "organization": ["My Org"],
+            "organizational_unit": ["My Dept"]
+        }
+    }
+
+Where `email` is the address to register with LetsEncrypt, and `ident` is the default CSR Subject
+identification info. This will allow the service to start up and generate a new LetsEncrypt
+account (storing the info in `{PREFIX}/auth`).
+
+See below for additional configuration options and their defaults. The two JSON keys above are the
+minimum to get a working setup.
+
+Cert Management
+---------------
+
+To request a cert and bring it under management, simply create a new (empty) key under `{PREFIX}/sites`:
+
+    $ curl --request PUT http://$CONSUL_ADDR/v1/kv/$CONSUL_PREFIX/sites/www.example.com
+
+This key by itself tells Porter to manage the specified domain's cert with default options: it will request
+if it's missing from its storage, and renew it on a schedule.
+
+You can also set a `sites/` key's value to a JSON document that overrides and customizes the options used. Any
+options available in the main `{PREFIX}/conf` key are avilable to be overridden here, with the exception of
+`email`.
+
+Once requested and acquired, certs and private keys are stored in `{PREFIX}/pki` as `www.example.com-cert.pem`
+and `www.example.com-key.pem`. This is to allow easy compatibility with
+[Fabio's Cert Stores](https://fabiolb.net/feature/certificate-stores/).
+
+### Revocation
+
+To revoke an issued cert, create a new empty key under `{PREFIX}/rev`:
+
+    $ curl --request PUT http://$CONSUL_ADDR/v1/kv/$CONSUL_PREFIX/rev/www.example.com
+
+This will request a certificate revocation from LE upstream, delete the stored private key and cert keys under
+`{PREFIX}/pki`, and then delete the triggering key from `{PREFIX}/rev` that you just created to prevent a loop.
+If an entry for this cert still exists under `{PREFIX}/sites`, Porter will attempt to aquire another.
+
+If you want to go scorched-earth, you can set the `{PREFIX}/rev` to a JSON document of `{"purge": true}`:
+
+    $ curl --request PUT --data '{"purge": true}' http://$CONSUL_ADDR/v1/kv/$CONSUL_PREFIX/rev/www.example.com
+
+This will do everything as described above, but will _also_ delete the entry under `{PREFIX}/sites` and revoke
+the cached LE authorization for the domain.
+
+Configuration
+-------------
+
+The following configuration variables are available to be set in the `{PREFIX}/conf` key and overridden
+in `sites/` unless otherwise noted. Defaults are shown below. Options with a `*` are required and
+have no default.
+
+    {
+        // Email address to register with LE
+        "email": *,
+
+        // CSR Subject values for requesting certs. All keys below are required and must be arrays.
+        "ident": {
+           "country": [*],
+           "province": [*],
+           "locality": [*],
+           "organization": [*],
+           "organizational_unit": [*]
+        },
+
+        // Time in days to wait before requesting a cert renewal. Must be 1 < x < 90.
+        "renew": 60,
+
+        // Additional names to request on this cert, making it a SAN cert (sites/ only).
+        "extra_names": []
+    }
---
+++ main.go
@@ -0,0 +1,271 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	consul "github.com/hashicorp/consul/api"
+	"golang.org/x/crypto/acme"
+	"gopkg.in/urfave/cli.v1"
+)
+
+const (
+	keyBits         = 2048
+	certDuration    = 90 * 24 * time.Hour // LE only offers 90-day certs
+	configKey       = "base_ident"
+	authKey         = "auth"
+	sitesKey        = "sites"
+	pkiKey          = "pki"
+	stagingEndpoint = "https://acme-staging.api.letsencrypt.org/directory"
+)
+
+type contextStoreKey int
+
+const (
+	ctxConsulClient contextStoreKey = iota
+	ctxConsulPrefix
+	ctxLetsEncryptEndpoint
+)
+
+type csrIdent struct {
+	Country            []string `json:"country"`
+	Province           []string `json:"province"`
+	Locality           []string `json:"locality"`
+	Organization       []string `json:"organization"`
+	OrganizationalUnit []string `json:"organizational_unit"`
+}
+
+func main() {
+	app := cli.NewApp()
+	app.HideHelp = true
+	app.UsageText = "porter [options]"
+	app.Before = cli.BeforeFunc(getConsulClient)
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "consul-addr, a",
+			Value:  "127.0.0.1:8500",
+			Usage:  "Connect to Consul running at `ADDR`",
+			EnvVar: "CONSUL_ADDR",
+		},
+		cli.StringFlag{
+			Name:   "consul-prefix, c",
+			Value:  "porter",
+			Usage:  "Consul KV store `PREFIX` for configuration and storage",
+			EnvVar: "CONSUL_PREFIX",
+		},
+		cli.BoolFlag{
+			Name:   "staging, s",
+			Usage:  "Use LetsEncrypt staging environment instead of production",
+			EnvVar: "LE_STAGING",
+		},
+		cli.StringFlag{
+			Name:   "infoblox-user, u",
+			Usage:  "Use `USER` to authenticate with the Infoblox API",
+			EnvVar: "INFOBLOX_USER",
+		},
+		cli.StringFlag{
+			Name:   "infoblox-pass, p",
+			Usage:  "Use `PASS` to authenticate with the Infoblox API",
+			EnvVar: "INFOBLOX_PASS",
+		},
+		cli.BoolFlag{
+			Name:  "help, h",
+			Usage: "show help",
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	if c.Bool("help") {
+		cli.ShowAppHelpAndExit(c, 0)
+		return nil
+	}
+
+	consulClient := c.App.Metadata["consul"].(*consul.Client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	ctx = context.WithValue(ctx, ctxConsulClient, consulClient)
+	ctx = context.WithValue(ctx, ctxConsulPrefix, c.String("consul-prefix"))
+	uri := acme.LetsEncryptURL
+	if c.Bool("staging") {
+		uri = stagingEndpoint
+	}
+	ctx = context.WithValue(ctx, ctxLetsEncryptEndpoint, uri)
+
+	_, err := getAcmeClient(ctx)
+	if err != nil {
+		cancel()
+		return err
+	}
+
+	cancel()
+
+	return nil
+}
+
+func getConsulClient(c *cli.Context) error {
+	client, err := consul.NewClient(&consul.Config{
+		Address: c.String("consul-addr"),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	c.App.Metadata["consul"] = client
+	return nil
+}
+
+// func do(c *cli.Context) error {
+// 	keyBytes, err := ioutil.ReadFile("key.pem")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	block, _ := pem.Decode(keyBytes)
+// 	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	client := &acme.Client{Key: key}
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+// 	defer cancel()
+// 	acct, err := client.GetReg(ctx, "")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("Account URI: %s\n", acct.URI)
+// 	fmt.Printf("Account Authz: %s\n", acct.Authz)
+
+// 	auth, err := client.Authorize(context.Background(), "ui.jlindsey.me")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	spew.Dump(auth)
+
+// 	if auth.Status != "valid" {
+// 		var dnsChallenge *acme.Challenge
+// 		for _, chal := range auth.Challenges {
+// 			if chal.Type == "dns-01" {
+// 				dnsChallenge = chal
+// 				break
+// 			}
+// 		}
+
+// 		if dnsChallenge == nil {
+// 			panic("no dns challenge")
+// 		}
+
+// 		dnsChallenge, err = client.Accept(context.Background(), dnsChallenge)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		s, err := client.DNS01ChallengeRecord(dnsChallenge.Token)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		spew.Dump(s)
+
+// 		dnsReqBody := map[string]string{
+// 			"type": "TXT",
+// 			"name": "_acme-challenge.ui",
+// 			"ttl":  "300",
+// 			"data": s,
+// 		}
+// 		body, err := json.Marshal(dnsReqBody)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		httpClient := &http.Client{}
+// 		req, err := http.NewRequest("POST", fmt.Sprintf(doDomainAPIURL, "jlindsey.me"), bytes.NewBuffer(body))
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		req.Header.Set("Content-Type", "application/json")
+// 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+// 		resp, err := httpClient.Do(req)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		defer resp.Body.Close()
+
+// 		respBody, err := ioutil.ReadAll(resp.Body)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		if resp.StatusCode != 201 {
+// 			panic(string(respBody))
+// 		}
+// 	}
+
+// 	goodAuth, err := client.WaitAuthorization(context.Background(), auth.URI)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	spew.Dump(goodAuth)
+
+// 	req := &x509.CertificateRequest{
+// 		SignatureAlgorithm: x509.SHA256WithRSA,
+// 		Subject: pkix.Name{
+// 			Country:            []string{"US"},
+// 			Province:           []string{"Virginia"},
+// 			Locality:           []string{"Arlington"},
+// 			Organization:       []string{"jlindsey.me"},
+// 			OrganizationalUnit: []string{"Me"},
+// 			CommonName:         "ui.jlindsey.me",
+// 		},
+// 	}
+
+// 	csr, err := x509.CreateCertificateRequest(rand.Reader, req, key)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	crt, crtURL, err := client.CreateCert(context.Background(), csr, 24*30*time.Hour, true)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("Cert URL: %s\n", crtURL)
+
+// 	crtBlock := &pem.Block{
+// 		Bytes: crt[0],
+// 		Type:  "CERTIFICATE",
+// 	}
+
+// 	err = ioutil.WriteFile("ui.jlindsey.me.crt", pem.EncodeToMemory(crtBlock), 0644)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	chainBlock := &pem.Block{
+// 		Bytes: make([]byte, 0),
+// 		Type:  "CERTIFICATE",
+// 	}
+
+// 	for _, cert := range crt[1:] {
+// 		for _, b := range cert {
+// 			chainBlock.Bytes = append(chainBlock.Bytes, b)
+// 		}
+// 	}
+
+// 	err = ioutil.WriteFile("ui.jlindsey.me.chain", pem.EncodeToMemory(chainBlock), 0644)
+// 	return nil
+// }
